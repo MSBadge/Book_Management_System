0b5d1837be89b8276eeb562d507f0f724ce39cff
MSBadge <badgemukul36@gmail.com>
2026-05-27 12:48:21 +0530

    update build

--- backend/build.sh
+++ backend/build.sh
@@ -5,3 +5,9 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
+
+
+if [[ $CREATE_SUPERUSER ]]
+then
+    python manage.py createsuperuser --no-input
+fi
