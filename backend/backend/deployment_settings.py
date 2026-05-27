f3e1b8dc2772a0509993b8516b66d17769cb3b07
MSBadge <badgemukul36@gmail.com>
2026-05-27 10:47:31 +0530

    backend update

--- backend/backend/deployment_settings.py
+++ backend/backend/deployment_settings.py
@@ -4,6 +4,8 @@ from backend.settings import *
 from .settings import BASE_DIR
 
 ALLOWED_HOSTS = [os.environ.get['RENDER_EXTERNAL_HOSTNAME']]
+ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
+
 
 CSRF_TRUSTED_ORIGINS = ['https://'+os.environ.get['RENDER_EXTERNAL_HOSTNAME']]
 
@@ -45,37 +47,4 @@ DATABASES = {
         conn_max_age=600
     )
 }
-
-
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'class': 'django.utils.log.AdminEmailHandler',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    },
-}
-
-
-
-ADMINS = [("CBI Analytics", "YOUREMAIL@EMAIL.com")]
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
-DEFAULT_FROM_EMAIL = 'default from email'
-
 
