675d9de598142aa1ef95a2536bd361879b24991e
MSBadge <badgemukul36@gmail.com>
2026-05-27 11:07:16 +0530

    same update

--- backend/backend/deployment_settings.py
+++ backend/backend/deployment_settings.py
@@ -7,7 +7,7 @@ ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
 ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
 
 
-CSRF_TRUSTED_ORIGINS = ['https://'+os.environ.get['RENDER_EXTERNAL_HOSTNAME']]
+CSRF_TRUSTED_ORIGINS = ['https://'+os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
 
 DEBUG = False
 
